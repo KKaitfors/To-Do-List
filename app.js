06e258782d1d74a86c2e143b5d621cd03007991c
KKaitfors <kevinkaitfors@gmail.com>
2022-10-15 10:38:40 -0600

    altered image reference address

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ window.addEventListener("load", () => {
 
     const task = input.value;
     if (!task) {
-      alert("Please enter a task")
+      alert("Please enter a task");
       return;
     }
 
@@ -28,43 +28,48 @@ window.addEventListener("load", () => {
 
     const task_edit_btn = document.createElement("button");
     task_edit_btn.classList.add("task-btn", "edit-btn");
-    task_edit_btn.innerHTML = "<img class='task-button-icon' src='images/edit-mark.png'/>Edit";
+    task_edit_btn.innerHTML =
+      "<img class='task-button-icon' src='/images/edit-mark.png'/>Edit";
 
     task_el.appendChild(task_edit_btn);
 
     const task_delete_btn = document.createElement("button");
     task_delete_btn.classList.add("task-btn", "delete-btn");
-    task_delete_btn.innerHTML = "<img class='task-button-icon' src='images/delete-mark.png'/>Delete";
+    task_delete_btn.innerHTML =
+      "<img class='task-button-icon' src='/images/delete-mark.png'/>Delete";
 
     task_el.appendChild(task_delete_btn);
 
     const task_complete_btn = document.createElement("button");
     task_complete_btn.classList.add("task-btn", "complete-btn");
-    task_complete_btn.innerHTML = "<img class='task-button-icon' src='images/check-mark.png'/>Complete";
+    task_complete_btn.innerHTML =
+      "<img class='task-button-icon' src='/images/check-mark.png'/>Complete";
 
     task_el.appendChild(task_complete_btn);
 
     input.value = "";
 
-    task_edit_btn.addEventListener('click', () => {
+    task_edit_btn.addEventListener("click", () => {
       if (task_edit_btn.innerText.toLowerCase() == "edit") {
         task_input_el.removeAttribute("readonly");
         task_input_el.focus();
-        task_edit_btn.innerHTML = "<img class='task-button-icon' src='images/save-mark.png'/>Save";
+        task_edit_btn.innerHTML =
+          "<img class='task-button-icon' src='/images/save-mark.png'/>Save";
       } else {
         task_input_el.setAttribute("readonly", "readonly");
-        task_edit_btn.innerHTML = "<img class='task-button-icon' src='images/edit-mark.png'/>Edit";
+        task_edit_btn.innerHTML =
+          "<img class='task-button-icon' src='/images/edit-mark.png'/>Edit";
       }
     });
 
-    task_delete_btn.addEventListener('click', () => {
+    task_delete_btn.addEventListener("click", () => {
       pending_tasks.removeChild(task_el);
     });
 
-    task_complete_btn.addEventListener('click', () => {
+    task_complete_btn.addEventListener("click", () => {
       const check = document.createElement("img");
       check.setAttribute("id", "check");
-      check.setAttribute("src", "images/check-mark.png");
+      check.setAttribute("src", "/images/check-mark.png");
       task_el.insertBefore(check, task_input_el);
       task_el.classList.remove("task-to-do");
       task_el.classList.add("finished-task");
